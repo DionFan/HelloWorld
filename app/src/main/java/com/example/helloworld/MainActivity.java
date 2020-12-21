31da8208ad2cbf733127b73957b9756818eb31b2
DionFan <dionisis_zn@hotmail.com>
2020-12-21 20:44:14 +0200

    2o sxolio

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.helloworld;//allages apo to branche
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;//allages apo to andorid studio
 
 import android.os.Bundle;
 
